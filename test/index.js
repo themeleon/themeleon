796013fabac3b57d368cdce4590b6789f13a154a
Valérian Galliat <val@codejam.info>
2014-10-11 12:57:49 +0200

    Use tape and faucet instead of nodeunit

--- test/index.js
+++ test/index.js
@@ -3,18 +3,15 @@
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
+var test = require('tape');
 
 var j = path.join;
 
 var THEME = path.resolve(__dirname, 'theme');
 var DEST = path.resolve(__dirname, 'dest');
 
-process.on('uncaughtException', function (e) {
-  console.error(e.stack);
-});
-
-exports.render = function (test) {
-  test.expect(3);
+test('render', function (t) {
+  t.plan(5);
 
   rimraf(DEST, function () {
     var themeleon = require('..')();
@@ -22,40 +19,36 @@ exports.render = function (test) {
     themeleon.use(j(THEME, 'mixin'), 'test');
     themeleon.use({object: 'object'});
 
-    var theme = themeleon(THEME, function (t) {
-      test.ok(t.test === 'test', 'Node.js module mixin (function)');
-      test.ok(t.object === 'object', 'Object mixin (literal)');
+    var theme = themeleon(THEME, function (th) {
+      t.equal(th.test, 'test', 'Node.js module mixin (function)');
+      t.equal(th.object, 'object', 'Object mixin (literal)');
 
-      t.base('foo/bar');
-      test.ok(t.ctx.base === './..', 'Base directory');
+      th.base('foo/bar');
+      t.equal(th.ctx.base, './..', 'Base directory');
 
-      t.copy('assets');
-      t.copy('assets', 'public');
+      th.copy('assets');
+      th.copy('assets', 'public');
     });
 
-    theme(DEST, {}).done(test.done);
+    theme(DEST, {}).then(function () {
+      t.ok(
+        compare(
+          j(THEME, 'assets/css/main.css'),
+          j(DEST, 'assets/css/main.css')
+        ),
+        'Copy an asset (same name)'
+      );
+
+      t.ok(
+        compare(
+          j(THEME, 'assets/css/main.css'),
+          j(DEST, 'public/css/main.css')
+        ),
+        'Copy an asset (rename)'
+      );
+    });
   });
-};
-
-exports.copy = function (t) {
-  t.ok(
-    compare(
-      j(THEME, 'assets/css/main.css'),
-      j(DEST, 'assets/css/main.css')
-    ),
-    'Copy an asset (same name)'
-  );
-
-  t.ok(
-    compare(
-      j(THEME, 'assets/css/main.css'),
-      j(DEST, 'public/css/main.css')
-    ),
-    'Copy an asset (rename)'
-  );
-
-  t.done();
-};
+});
 
 function compare(src, dest) {
   return cat(src) === cat(dest);
